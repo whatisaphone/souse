0de869ef4510c92b90296a27faba36fba262af6f
John Simon <john@johnsoft.com>
2012-06-04 05:09:46 -0500

    Added a tray icon that appears when the main form is minimized

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,11 +29,53 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
+			this.mnuNotifyIcon = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.mnuShow = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuNotifyIcon.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// mnuNotifyIcon
+			// 
+			this.mnuNotifyIcon.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuShow,
+            this.mnuExit});
+			this.mnuNotifyIcon.Name = "mnuNotifyIcon";
+			this.mnuNotifyIcon.Size = new System.Drawing.Size(118, 48);
+			// 
+			// mnuShow
+			// 
+			this.mnuShow.Name = "mnuShow";
+			this.mnuShow.Size = new System.Drawing.Size(152, 22);
+			this.mnuShow.Text = "&Show";
+			this.mnuShow.Click += new System.EventHandler(this.mnuShow_Click);
+			// 
+			// mnuExit
+			// 
+			this.mnuExit.Name = "mnuExit";
+			this.mnuExit.Size = new System.Drawing.Size(152, 22);
+			this.mnuExit.Text = "E&xit";
+			this.mnuExit.Click += new System.EventHandler(this.mnuExit_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(292, 273);
+			this.Name = "MainForm";
 			this.Text = "Form1";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+			this.Resize += new System.EventHandler(this.MainForm_Resize);
+			this.mnuNotifyIcon.ResumeLayout(false);
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.ContextMenuStrip mnuNotifyIcon;
+		private System.Windows.Forms.ToolStripMenuItem mnuShow;
+		private System.Windows.Forms.ToolStripMenuItem mnuExit;
 	}
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -1,12 +1,60 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace MouseAhead
 {
 	public partial class MainForm : Form
 	{
+		NotifyIcon notifyIcon;
+
 		public MainForm()
 		{
 			InitializeComponent();
+			notifyIcon = new NotifyIcon();
+			notifyIcon.Text = "MouseAhead";
+			notifyIcon.Icon = this.Icon;
+			notifyIcon.ContextMenuStrip = mnuNotifyIcon;
+			notifyIcon.DoubleClick += NotifyIcon_DoubleClick;
+		}
+
+		private void mnuShow_Click(object sender, EventArgs e)
+		{
+			ShowMe();
+		}
+
+		private void MainForm_Resize(object sender, EventArgs e)
+		{
+			if (WindowState == FormWindowState.Minimized)
+				HideMe();
+		}
+
+		private void NotifyIcon_DoubleClick(object sender, EventArgs e)
+		{
+			ShowMe();
+		}
+
+		private void ShowMe()
+		{
+			notifyIcon.Visible = false;
+			Show();
+			WindowState = FormWindowState.Normal;
+			Activate();
+		}
+
+		private void HideMe()
+		{
+			Hide();
+			notifyIcon.Visible = true;
+		}
+
+		private void mnuExit_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+
+		private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			notifyIcon.Visible = false;
 		}
 	}
 }
